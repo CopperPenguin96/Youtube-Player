82eafde093e67acd27f004eb7df99a1f16480882
apotter96 <apotter96@godispower.com>
2018-06-08 15:07:19 -0400

    More Fixes

---
+++ Youtube-Player-Examplef/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Youtube_Player_Examplef
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.player1 = new YoutubePlayer.Player();
+            this.SuspendLayout();
+            // 
+            // player1
+            // 
+            this.player1.Location = new System.Drawing.Point(153, 29);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(577, 419);
+            this.player1.TabIndex = 0;
+            this.player1.VideoID = "NpEaa2P7qZI";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 491);
+            this.Controls.Add(this.player1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private YoutubePlayer.Player player1;
+    }
+}
+
