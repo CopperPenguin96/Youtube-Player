671bb177e9fae919142b80405b59a948012551d4
apotter96 <apotter96@godispower.com>
2018-06-08 14:30:09 -0400

    Fixed Crashing on Designer
    Fixed a crash that occured when adding the control to the designer.

---
+++ YoutubePlayer/Player.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace YoutubePlayer
+{
+    partial class Player
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.geckoWebBrowser1 = new Gecko.GeckoWebBrowser();
+            this.SuspendLayout();
+            // 
+            // geckoWebBrowser1
+            // 
+            this.geckoWebBrowser1.Enabled = false;
+            this.geckoWebBrowser1.FrameEventsPropagateToMainWindow = false;
+            this.geckoWebBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.geckoWebBrowser1.Name = "geckoWebBrowser1";
+            this.geckoWebBrowser1.Size = new System.Drawing.Size(577, 419);
+            this.geckoWebBrowser1.TabIndex = 0;
+            this.geckoWebBrowser1.UseHttpActivityObserver = false;
+            this.geckoWebBrowser1.Visible = false;
+            // 
+            // Player
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.geckoWebBrowser1);
+            this.Name = "Player";
+            this.Size = new System.Drawing.Size(577, 419);
+            this.Load += new System.EventHandler(this.Player_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Gecko.GeckoWebBrowser geckoWebBrowser1;
+    }
+}
---
+++ YoutubePlayer/Player.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Gecko.Events;
+using System.Drawing;
+
+namespace YoutubePlayer
+{
+    public partial class Player : UserControl
+    {
+        string id = "NpEaa2P7qZI";
+
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        [Bindable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public string VideoID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                if (!designMode) geckoWebBrowser1.LoadHtml(GetHtml());
+            }
+        }
+
+        private string GetUrl()
+        {
+            return $"https://youtube.com/v/{id}?autoplay=0&version=3";
+        }
+        
+        private bool designMode;
+        public Player()
+        {
+            InitializeComponent();
+            designMode = LicenseManager.UsageMode == LicenseUsageMode.Designtime;
+            try
+            {
+                if (designMode) // If in design mode disables browser and puts a dummy cover
+                {
+                    Label label1 = new Label()
+                    {
+                        Width = this.Width,
+                        Height = this.Height,
+                        Location = new System.Drawing.Point(0, 0),
+                        Text = "YoutubePlayer Control",
+                        Font = new System.Drawing.Font("Times New Roman", 23),
+                        TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                        AutoSize = false,
+                        BorderStyle = BorderStyle.Fixed3D,
+                        BackColor = Color.Maroon
+                    };
+                    this.Controls.Add(label1);
+                }
+                else
+                {
+                    Console.WriteLine("Initializing Xulrunner...");
+                    Gecko.Xpcom.Initialize("firefox\\");
+                    Gecko.CertOverrideService.GetService().ValidityOverride += geckoWebBrowser1_ValidityOverride;
+                    geckoWebBrowser1.Enabled = true;
+                    geckoWebBrowser1.Visible = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                Console.WriteLine("Unable to proceed: " + ex.ToString());
+            }
+        }
+
+        private void Player_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Youtube Player Loaded ({Name})");
+        }
+        private void geckoWebBrowser1_ValidityOverride(object sender, CertOverrideEventArgs e)
+        {
+            e.OverrideResult = Gecko.CertOverride.Mismatch | Gecko.CertOverride.Time | Gecko.CertOverride.Untrusted;
+            e.Temporary = true;
+            e.Handled = true;
+        }
+
+        private string GetHtml()
+        {
+            return @"
+<html>
+    <head>
+        <style type='text/css'>
+            body, html
+            {
+                margin: 0; padding: 0; height: 100%; overflow: hidden;
+            }
+
+            #content
+            {
+                position:absolute; left: 0; right: 0; bottom: 0; top: 0px; 
+            }
+        </style>
+    </head>
+    <body>
+        <div id='content'>
+            <iframe id='ytplayer' type='text/html' width='100%' height='100%' frameborder='0'
+  src='http://www.youtube.com/embed/" + VideoID + @"?autoplay=0&version=3'
+  frameborder='0'></iframe>
+        </div>
+    </body>
+</html>";
+        }
+
+        
+
+    }
+	public class ControlNotEnabledException : Exception
+        {
+            public ControlNotEnabledException() : base()
+            {
+
+            }
+
+            public ControlNotEnabledException(string message) : base(message)
+            {
+
+            }
+
+            public ControlNotEnabledException(string message, Exception inner) : base(message, inner)
+            {
+
+            }
+
+            public ControlNotEnabledException(Exception inner) : base("", inner)
+            {
+
+            }
+        }
+}
